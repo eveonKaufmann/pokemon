6d502fe3b504d5da32acc709b3111aa7fb3428bf
Unknown <j.ahameafule@gmx.ch>
2018-07-19 13:17:40 +0200

    Generated Constructors,  Getters & Setters for Trainer Class

--- src/main/java/ch/nyp/pokemon/trainer/Trainer.java
+++ src/main/java/ch/nyp/pokemon/trainer/Trainer.java
@@ -10,5 +10,41 @@ public class Trainer {
 	private int wins;
 	private int losses;
 	private ArrayList<Pokemon> pokemonList;
+	
+	public Trainer() {
+
+	}
+	
+  public Trainer(String name, int wins, int losses, ArrayList<Pokemon> pokemonList) {
+    this.name = name;
+    this.wins = wins;
+    this.losses = losses;
+    this.pokemonList = pokemonList;
+  }
+
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public int getWins() {
+    return wins;
+  }
+  public void setWins(int wins) {
+    this.wins = wins;
+  }
+  public int getLosses() {
+    return losses;
+  }
+  public void setLosses(int losses) {
+    this.losses = losses;
+  }
+  public ArrayList<Pokemon> getPokemonList() {
+    return pokemonList;
+  }
+  public void setPokemonList(ArrayList<Pokemon> pokemonList) {
+    this.pokemonList = pokemonList;
+  }
 
 }
